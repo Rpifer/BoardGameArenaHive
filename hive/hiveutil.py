e28d7756e89a3ba350e296e780687de39fc75d0e
Pifer <rpifer@udel.edu>
2020-06-27 22:47:06 -0400

    overhauled hiveutil to consider movement better

--- hive/hiveutil.py
+++ hive/hiveutil.py
@@ -1,5 +1,9 @@
 from hive import piece, board, hexutil
-from typing import List
+from typing import List, Any
+from collections import deque, namedtuple
+import math
+
+Move = namedtuple("Move", ["position", "direction"])
 
 
 def is_one_hive(tiles: List[board.Tile]):
@@ -62,26 +66,73 @@ def space_crawable(start: board.Tile, end: hexutil.Point, tiles: List[board.Tile
     tiles = tiles[:]  # duplicate so to not change original
     # remove self from tiles to not interfere with movement
     tiles.remove(start)
-    possible_paths = [[hexutil.Point(start.x, start.y)]]
-    visited = [hexutil.Point(start.x, start.y)]
 
+    possible_paths = [deque()]
+    possible_paths[0].append(Move(hexutil.Point(start.x, start.y), 0))
+
+    current_path = possible_paths[0]
     spots = start.piece.crawl_spaces or 50
     for i in range(1, spots + 1):
-        possible_paths.append([])
-        for p in possible_paths[i-1]:
-            neighbors = hexutil.touching_hexagons(p)
+        paths_to_extend = possible_paths
+        p: deque[Move]
+        for p in paths_to_extend:
+            path_to_be_removed = p
+            neighbors = hexutil.touching_hexagons(p[-1].position)
             for n in neighbors:
-                if n in movement_cloud and can_slide_to(p, n, tiles) \
-                        and (n not in visited or start.piece.crawl_spaces is not None): # stop ant from going exponential
+                if n in movement_cloud and can_slide_to(p, n, tiles):  # not (dir != p[-1]dir and not p[-2].hex == n)
                     if n == end and start.piece.crawl_spaces is None:
                         return True
-                    visited.append(n)
+                    # todo: check direction in if and then append new path with p + deque(move)
                     possible_paths[i].append(n)
                 elif n == end:
                     return False
 
     return end in possible_paths[spots]
     # check closest path that can be reached via traversal rules, ie can slide
+
+
+def direction_of_crawl(start: hexutil.Point, end: hexutil.Point, tiles: List[board.Tile]):
+    # find neighbors of start
+    start_neighbors = hexutil.touching_hexagons(start)
+    # find neighbors of end
+    end_neighbors = hexutil.touching_hexagons(end)
+    # find union
+    intersecting_neighbors = list(set(start_neighbors) & set(end_neighbors))
+    static_piece = None
+    # find one of those that is a piece
+    for p in intersecting_neighbors:
+        match = next((i for i in range(0, len(tiles)) if tiles[i].x == p.x and tiles[i].y == p.y), None)
+        if match is not None:
+            static_piece = p
+    # calculate angle of start and angle of end in relation to that piece
+    x_start = hexutil.relative_distance_x(static_piece, start)
+    y_start = hexutil.relative_distance_y(static_piece, start)
+    x_end = hexutil.relative_distance_x(static_piece, end)
+    y_end = hexutil.relative_distance_y(static_piece, end)
+    # find angle of start
+    t_start = angle_of_vector(x_start, y_start)
+    t_end = angle_of_vector(x_end, y_end)
+    return (1, -1)[t_end > t_start]  # if increasing, then counter clockwise then -1
+
+
+def angle_of_vector(x, y):
+    # backwards due to orientation of axis
+    y = -y
+    if y == 0:
+        return int(x < 0) * 180
+
+    if x == 0:
+        return int(y > 0) * 180 + 90
+
+    theta = math.degrees(math.atan(abs(y/x)))
+    if x < 0 < y:
+        theta += 90
+    elif x < 0 and y < 0:
+        theta += 180
+    elif x > 0 > y:
+        theta += 270
+
+    return theta
 
 
 def can_slide_to(start: hexutil.Point, end: hexutil.Point, tiles: List[board.Tile]):
@@ -130,3 +181,11 @@ def can_start_crawl(start: hexutil.Point, tiles: List[board.Tile]):
         return True
     else:
         return False
+
+
+t = [
+    board.Tile(2, 0, 0, piece.create_ladybug('W')),
+    board.Tile(2, 1, 0, piece.create_ladybug('W'))
+]
+first = t.pop(0)
+print(direction_of_crawl(hexutil.Point(3, 0), hexutil.Point(3, 1), t))
